5f8db66691328b9944168b1b9d8400858e9d9cc3
Eric Araujo <ericalbuquerque8@hotmail.com>
2025-11-05 19:30:06 -0300

    adicionando dockerfile e docker-compose

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DiskEntulho_PI4
+| docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=1234" -p 1433:1433 --name sqlserver2022 -d mcr.microsoft.com/mssql/server:2022-latest 
---
+++ backend/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+// Configura o pipeline HTTP
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
